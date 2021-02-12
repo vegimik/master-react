48c7691cd7ea52e96c823cfc2c43398dfc02f4df
Vegim Karaçica <vegimi.k@hotmail.com>
2021-02-12 08:41:56 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+function App() {
+    return <div>Hi there!</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
